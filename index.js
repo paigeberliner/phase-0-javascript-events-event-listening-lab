bfb4e6ef5490f84aab41502b5fe8c3962987f8d0
Paige <paigeberliner@gmail.com>
2023-08-17 15:12:58 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function addingEventListener() {
-    const input = document.getElementById('button');
-    input.addEventListener('click', function() {
-      alert('I was clicked!');
-    });
-  }
-  
-  addingEventListener();
+  const input = document.getElementById('button');
+  input.addEventListener('click', function() {
+    alert('I was clicked!');
+  });
+}
+
+addingEventListener();
